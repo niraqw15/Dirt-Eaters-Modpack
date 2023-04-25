687a0f92a8ae828f77e3775b15f9ae7d24083c6e
Nathan Quick Wright <niraqw@gmail.com>
2023-04-24 17:50:58 -0700

    Update pack version to 1.1

--- Manifest_Generator.py
+++ Manifest_Generator.py
@@ -24,7 +24,7 @@ mcVersion = config["loader"]["mcVersion"]
 loaderVersion = config["loader"]["loaderVersion"]
 
 packName = "Dirt Eaters Modpack"
-packVersion = "1.0"
+packVersion = "1.1"
 packAuthor = "niraqw15"
 
 manifest = {
